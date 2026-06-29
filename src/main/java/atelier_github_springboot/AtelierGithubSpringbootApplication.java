03849a252edaf995f99075fb9a4431dbd343fc53
Sabrina Tantin <sabrina.tantin@icloud.com>
2026-06-29 10:42:21 +0200

    first commit

---
+++ src/main/java/atelier_github_springboot/AtelierGithubSpringbootApplication.java
@@ -0,0 +1,13 @@
+package atelier_github_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtelierGithubSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtelierGithubSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/atelier_github_springboot/AtelierGithubSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package atelier_github_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtelierGithubSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
