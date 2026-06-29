b2274159ed9e36aea7d6f865179e59bb03f547f8
SabrinaTantin <cyber.alter.2027@gmail.com>
2026-06-29 11:47:01 +0200

    Ajout controleur REST HelloController

---
+++ src/main/java/atelier_github_springboot/HelloController.java
@@ -0,0 +1,13 @@
+package atelier_github_springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot depuis Docker et GitHub Actions";
+    }
+}
